f4757f6673c4cd10a4f8075015299adf01d307f2
sisifodev <manuelperdomo1710@icloud.com>
2021-11-06 10:48:46 -0500

    Login component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import "../styles/global.scss";
+import Layout from "../containers/Layout";
+import Login from "../containers/Login";
+import "../styles/global.css";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hola Mundo!</h1>
-      <p>This is my first app react with webpack!!! it is beautiful</p>
-    </div>
+    <Layout>
+      <Login />
+    </Layout>
   );
 };
 
