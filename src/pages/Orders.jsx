12b6c8bc2e438634313eb253ce18de391060b2f2
Manuel Perdomo <manuelperdomo1710@icloud.com>
2021-11-07 07:37:16 -0500

    All components

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React from 'react';
+import OrderItem from '../components/OrderItem';
+import '../styles/Orders.scss';
 
 const Orders = () => {
-  return (
-    <div>
-      <h1>Orders page</h1>
-    </div>
-  );
-};
+	return (
+		<div className="Orders">
+			<div className="Orders-container">
+				<h1 className="title">My orders</h1>
+				<div className="Orders-content">
+					<OrderItem />
+				</div>
+			</div>
+		</div>
+	);
+}
 
 export default Orders;
