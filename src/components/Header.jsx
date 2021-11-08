96909a1cc900c37825b57c3101670adc409e8581
Manuel Perdomo <manuelperdomo1710@icloud.com>
2021-11-08 10:03:48 -0500

    useState

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "@styles/Header.scss";
-
+import Menu from "@components/Menu";
 import menu from "@icons/icon_menu.svg";
 import logo from "@logos/logo_yard_sale.svg";
 import shopingCart from "@icons/icon_shopping_cart.svg";
 
 const Header = () => {
+  const [toogle, setToogle] = useState(false);
+
+  const handleToogle = (state) => {
+    setToogle(!toogle);
+  };
+
   return (
     <nav>
       <img src={menu} alt="menu" className="menu" />
@@ -34,13 +40,16 @@ const Header = () => {
       </div>
       <div className="navbar-right">
         <ul>
-          <li className="navbar-email">platzi@example.com</li>
+          <li className="navbar-email" onClick={handleToogle}>
+            platzi@example.com
+          </li>
           <li className="navbar-shopping-cart">
             <img src={shopingCart} alt="shopping cart" />
             <div>2</div>
           </li>
         </ul>
       </div>
+      {toogle && <Menu />}
     </nav>
   );
 };
