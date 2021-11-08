9266f6ec40096ca7b2f619fed26e881f90f7229d
Manuel Perdomo <manuelperdomo1710@icloud.com>
2021-11-08 10:35:50 -0500

    Displaying products from the API

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProductItem from "../components/ProductItem";
 import "@styles/ProductList.scss";
+import axios from "axios";
 
 const ProductList = () => {
+  const [products, setProducts] = useState([]);
+  const API = "http://api.escuelajs.co/api/v1/products";
+  useEffect(async () => {
+    const response = await axios(API);
+    setProducts(response.data);
+  }, []);
+
   return (
     <section className="main-container">
       <div className="ProductList">
-        <ProductItem />
+        {products.map((product) => (
+          <ProductItem
+            price={product.price}
+            productName={product.title}
+            image={product.images[0]}
+          />
+        ))}
       </div>
     </section>
   );
